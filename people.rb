e1b804ad47bfbaa82fe69d754d817eb94bc82656
mwraich <mwraich28@gmail.com>
2016-01-14 14:53:27 -0500

    Comepleted exercise, added both instructor and student classes and respective methods as outlined

--- people.rb
+++ people.rb
@@ -1,9 +1,32 @@
-# 1
-class Instructor
-end
+# # 1
 
-class Student
-  def learn
-    puts "I get it!" #2
+# module Greeting
+#   puts "Hi, my name is #{name}"
+# end
+
+class Person
+attr_accessor :name
+def initialize(name)
+  @name = name
+  puts "Hi, my name is #{name}"
   end
 end
+
+class Instructor < Person
+  def teach
+    puts "Everything in Ruby is an Object."
+  end
+end
+
+class Student < Person
+  def learn
+    puts "I get it!"
+  end
+end
+
+chris = Instructor.new("Chris")
+christina = Student.new("Christina")
+chris.teach
+christina.learn
+#christina.teach => This produces and error because that method is restricted to
+# the instructor class and as a student she cannot access that method
